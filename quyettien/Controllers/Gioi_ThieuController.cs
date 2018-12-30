c395cb02139bccc8c1e47692afbe79e834ad3dba
hoaiphatcr <hoaiphat2010@gmail.com>
2018-12-31 00:29:04 +0700

    Page About Us

---
+++ quyettien/Controllers/Gioi_ThieuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quyettien.Controllers
+{
+    public class Gioi_ThieuController : Controller
+    {
+        // GET: Gioi_Thieu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
