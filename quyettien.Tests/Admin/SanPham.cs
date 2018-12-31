643d17811b507d6bd03700cbe66c937543eef28d
hoaiphatcr <hoaiphat2010@gmail.com>
2018-12-31 19:03:01 +0700

    add test project

---
+++ quyettien.Tests/Admin/SanPham.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using quyettien.Areas.admin.Controllers;
+using quyettien.Models;
+
+namespace quyettien.Tests.Admin
+{
+    [TestClass]
+    public class SanPham
+    {
+        [TestMethod]
+        public void Index()
+        {
+            var controller = new SanPhamController();
+            var result = controller.Index() as ViewResult;
+            var db = new DIENMAYQUYETTIENEntities();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(List<Product>));
+            Assert.AreEqual(db.Products.Count(), ((List<Product>)result.Model).Count);
+        }
+    }
+}
---
+++ quyettien.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("quyettien.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("quyettien.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f902fea7-d781-402e-890d-e8ecd7de3f80")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
