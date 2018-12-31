1159419e565efa8a18cf5f291ad754d0d747eab7
MoonNguyen2030 <nhunguyen1998@gmail.com>
2018-12-31 19:19:48 +0700

    Unit test danh sách danh mục

---
+++ quyettien.Tests/Admin/DanhMuc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using quyettien.Areas.admin.Controllers;
+using quyettien.Models;
+
+namespace quyettien.Tests.Admin
+{
+    [TestClass]
+    public class DanhMuc
+    {
+        [TestMethod]
+        public void TestIndexDanhMuc()
+        {
+            var controller = new DanhMucController();
+            var result = controller.Index() as ViewResult;
+            var db = new DIENMAYQUYETTIENEntities();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(List<ProductType>));
+
+        }
+    }
+}
