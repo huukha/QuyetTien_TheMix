f729639d30a5eeb5c0d27a6fb35106dcc59673c6
Kha <kha@email.com>
2018-12-31 23:55:08 +0700

    Final

---
+++ quyettien/Controllers/Lien_HeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quyettien.Controllers
+{
+    public class Lien_HeController : Controller
+    {
+        // GET: Lien_He
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
